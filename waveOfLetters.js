92ef848aa2a2fab3d7762b63ba388bb7225901a0
Bentrend <107846393+Bentrend@users.noreply.github.com>
2022-11-02 09:07:49 +0300

    Create waveOfLetters.js

---
+++ waveOfLetters.js
@@ -0,0 +1,16 @@
+function wave(str) {
+  let string = str.toLowerCase();           // приводим все в нижний регистр, чтобы сразу исключить заглавные буквы в строке
+  let result = [];                          // результирующий массив
+  
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == " ") {                 // пропускаем пробелы
+      continue;                             // переходим к следующей итерации
+    }
+    result.push(
+      string.slice(0, i) + string[i].toUpperCase() + string.slice(i + 1) // склеиваем строку и отправляем в результат
+    );
+  }
+  return result;
+}
+
+console.log(wave("codewars"));
